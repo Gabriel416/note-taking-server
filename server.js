9ad8a2036f7500bb9e7b0ea46b67c5a524330043
Gabriel Ferguson <gferguson954@gmail.com>
2021-04-04 02:07:27 -0500

    heroku port

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const { ApolloServer } = require('apollo-server');
 const { resolvers, typeDefs } = require('./schema');
 
 const server = new ApolloServer({ resolvers, typeDefs });
-server.listen({ port: 4000 }, () => {
+server.listen({ port: process.env.PORT || 4000 }, () => {
     console.log('listening on post 4000')
 });
